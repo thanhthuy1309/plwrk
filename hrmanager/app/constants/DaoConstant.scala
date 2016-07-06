a9d174f2b08c5cc8bc07671452ced1390c497ad2
dungpt2 <dungpt2@runsystem.net>
2016-07-06 09:30:56 +0700

    add

--- hrmanager/app/constants/DaoConstant.scala
+++ hrmanager/app/constants/DaoConstant.scala
@@ -1,9 +1,9 @@
 package constants
 object DaoConstant {
   val DEFAULT_PERSISTENCE_UNIT: String = "defaultPersistenceUnit"
-  var ROLE_DAO_FIND_ROLE_ALL = "RoleDao.findRoleAll"
-  var ROLE_DAO_FIND_ROLE_ID = "RoleDao.findRoleById"
+  var ROLE_DAO_FIND_ROLE_ALL: String = "RoleDao.findRoleAll"
+  var ROLE_DAO_FIND_ROLE_ID: String = "RoleDao.findRoleById"
 
-  var USER_DAO_FIND_USER_ALL = "UserDao.findUserAll"
-  var USER_DAO_FIND_EMAIL = "UserDao.findUserByEmail"
+  var USER_DAO_FIND_USER_ALL: String = "UserDao.findUserAll"
+  var USER_DAO_FIND_EMAIL: String = "UserDao.findUserByEmail"
 }
